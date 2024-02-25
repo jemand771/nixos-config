e27f6e53e13c3d26c2c2e26d98a6cb92099c27e9
Willy <jemand771@gmx.net>
2024-02-25 22:57:18 +0100

    install headlamp from appimage

--- configuration.nix
+++ configuration.nix
@@ -102,6 +102,14 @@
       lutris
       yt-dlp
       jetbrains-toolbox
+      # todo headlamp desktop file
+      (pkgs.appimageTools.wrapType1 {
+        name = "headlamp";
+        src = pkgs.fetchurl {
+          url = "https://github.com/headlamp-k8s/headlamp/releases/download/v0.22.0/Headlamp-0.22.0-linux-x64.AppImage";
+          hash = "sha256-KA4lxfiZLkkMANOg77n9mANTYXI+BJRwGUa1TRalFGM=";
+        };
+      })
     ];
   };
 

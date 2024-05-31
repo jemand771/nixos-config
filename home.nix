6578093c24c34d8219cbcc2db3e158eab3995edd
Willy <jemand771@gmx.net>
2024-05-31 18:13:13 +0200

    add kwin config fix against notification lagspikes

--- home.nix
+++ home.nix
@@ -9,6 +9,14 @@
     userEmail = "jemand771@gmx.net";
   };
 
+  home.file."${config.xdg.configHome}/plasma-workspace/env/kwin.sh" = {
+    text = ''
+      #/usr/bin/env sh
+      export __GL_YIELD="usleep"
+    '';
+    executable = true;
+  };
+
   home.stateVersion = "23.11";
   programs.home-manager.enable = true;
 }

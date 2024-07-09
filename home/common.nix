d680f10239f8f4f53aff0215b88df87d4b1575b4
Willy <jemand771@gmx.net>
2024-07-09 23:40:42 +0200

    start managing my ssh config through nix
    this is still missing lots of host because I don't have my main system at the moment, but it's a start

--- home/common.nix
+++ home/common.nix
@@ -11,6 +11,37 @@
   programs.htop.settings = {
     hide_userland_threads = 1;
   };
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        identityFile = "~/.ssh/id_github";
+      };
+
+      # TODO make these generic-ish per key/host group
+      "d39s-sx" = {
+        hostname = "88.99.58.198";
+        user = "root";
+        identityFile = "~/.ssh/id_d39s";
+      };
+      "d39s-sxvm" = {
+        hostname = "88.99.58.196";
+        user = "root";
+        identityFile = "~/.ssh/id_d39s";
+      };
+      "d39s-sxlh" = {
+        hostname = "10.0.2.4";
+        user = "root";
+        identityFile = "~/.ssh/id_d39s";
+        proxyJump = "d39s-sx";
+      };
+      "d39s-spg" = {
+        hostname = "168.119.251.136";
+        user = "root";
+        identityFile = "~/.ssh/id_d39s";
+      };
+    };
+  };
 
   programs.home-manager.enable = true;
 }

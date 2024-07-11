87d4fbc25ee804b4ec457dbca07b36c580b21fd0
Willy <jemand771@gmx.net>
2024-07-11 23:04:00 +0200

    sync repos folder
    this is very much a test/poc - syncing git repos when both sides have been edited may corrupt the repo

--- sync.nix
+++ sync.nix
@@ -13,6 +13,15 @@
         "nixbox2" = { id = "3E5EDPI-MNYTGMO-OFB3WSZ-GQZPDHR-X2WSUZ7-UUI22ET-2XS37YR-WW6HBAE"; };
         "nixtique" = { id = "YK257QH-FXBWGJK-EXJJ7TN-NRCJZ4Y-TOHZSO6-N6GZA7P-ZL3EWRR-SIORDQU"; };
       };
+      folders = {
+        "repos" = {
+          path = "/home/willy/repos";
+          devices = [
+            "nixbox2"
+            "nixtique"
+          ];
+        };
+      };
     };
   };
 }

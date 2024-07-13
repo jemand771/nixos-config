9697c1dfa1147a86d1c88fec43f9962536e5c73f
Willy <jemand771@gmx.net>
2024-07-13 15:13:06 +0200

    add my first email account to thunderbird

--- home/thunderbird.nix
+++ home/thunderbird.nix
@@ -2,7 +2,27 @@
 {
   programs.thunderbird = {
     enable = true;
-    profiles = {
+    profiles.default.isDefault = true;
+  };
+  # a note on passwords:
+  # while I could _probably_ set up some sort of keyring and shove in my mail passwords automatically,
+  # I don't really feel up for that task at the moment.
+  # Instead, I let thunderbird prompt me for the password on first launch (and first time sending an email)
+  # and tick the "save password" checkbox. Not entirely declarative, but good enough for now.
+  accounts.email.accounts."willy.hille@d39s.de" = {
+    address = "willy.hille@d39s.de";
+    userName = "willy.hille@d39s.de";
+    realName = "Willy Hille";
+    imap = {
+      host = "mail.d39s.de";
+      port = 993;
     };
+    smtp = {
+      host = "mail.d39s.de";
+      port = 465;
+    };
+    # TODO not really
+    primary = true;
+    thunderbird.enable = true;
   };
 }

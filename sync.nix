a2df7a8e20a670840d97c76ea503afc2bd77550d
Willy <jemand771@gmx.net>
2024-07-22 23:26:51 +0200

    add syncthing-arbiter to trusted devices

--- sync.nix
+++ sync.nix
@@ -15,6 +15,7 @@
         "nixbox" = { id = "CVWZPMJ-PTH5N4I-ACO6UFI-J5WOH4X-TMLEAUH-3QFH4C7-7X7NRJX-G3RH7AP"; };
         "nixbox2" = { id = "3E5EDPI-MNYTGMO-OFB3WSZ-GQZPDHR-X2WSUZ7-UUI22ET-2XS37YR-WW6HBAE"; };
         "nixtique" = { id = "YK257QH-FXBWGJK-EXJJ7TN-NRCJZ4Y-TOHZSO6-N6GZA7P-ZL3EWRR-SIORDQU"; };
+        "syncthing-arbiter" = { id = "CPMLFLH-2U6G6LI-GESKBA7-TG2I2KD-FZ63AHU-NGHMXBH-DL4PGG5-EU3CIQJ"; };
       };
       folders = {
         "repos" = {
@@ -23,6 +24,7 @@
             "nixbox"
             "nixbox2"
             "nixtique"
+            "syncthing-arbiter"
           ];
         };
       };

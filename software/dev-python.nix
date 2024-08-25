3be164ad54ad0ac20341424176587289c59144b5
Willy <jemand771@gmx.net>
2024-08-25 19:56:16 +0200

    python: install beautifulsoup4

--- software/dev-python.nix
+++ software/dev-python.nix
@@ -3,6 +3,7 @@
   options.jemand771.dev-python.enable = lib.mkEnableOption "python development";
   config.environment.systemPackages = with pkgs; lib.mkIf config.jemand771.dev-python.enable [
     (python3.withPackages (ps: with ps; [
+      beautifulsoup4
       pandas
       requests
       tabulate

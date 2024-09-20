5b6f78314b836d73c0e11ce534b9c8b1a8e7c0ed
Willy <jemand771@gmx.net>
2024-09-20 17:15:46 +0200

    install comic-neue (comic sans)

--- software/office-utils.nix
+++ software/office-utils.nix
@@ -2,14 +2,19 @@
 
 {
     options.jemand771.office-utils.enable = lib.mkEnableOption "office utils";
-    config.environment.systemPackages = with pkgs; lib.mkIf config.jemand771.office-utils.enable [
-        kcalc
-        libreoffice-qt
-        pinta
-        spotify
-        spotify-tray
-        texliveFull
-        unstable.vesktop
-        vlc
-    ];
+    config = lib.mkIf config.jemand771.office-utils.enable {
+        environment.systemPackages = with pkgs; [
+            kcalc
+            libreoffice-qt
+            pinta
+            spotify
+            spotify-tray
+            texliveFull
+            unstable.vesktop
+            vlc
+        ];
+        fonts.packages = with pkgs; [
+            comic-neue
+        ];
+    };
 }

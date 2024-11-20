b5bcc3803089c282f7850bea607e1e2b3ed633f1
Willy <jemand771@gmx.net>
2024-11-20 15:40:35 +0100

    nix-cache: set chunking options from guide

--- software/nix-cache.nix
+++ software/nix-cache.nix
@@ -5,5 +5,35 @@
 
     # TODO this should ideally live in /etc and be managed by agenix
     environmentFile = "/root/atticd.env";
+    
+    # defaults from https://docs.attic.rs/admin-guide/deployment/nixos.html
+    settings = {
+      listen = "[::]:8080";
+
+      jwt = { };
+
+      # Data chunking
+      #
+      # Warning: If you change any of the values here, it will be
+      # difficult to reuse existing chunks for newly-uploaded NARs
+      # since the cutpoints will be different. As a result, the
+      # deduplication ratio will suffer for a while after the change.
+      chunking = {
+        # The minimum NAR size to trigger chunking
+        #
+        # If 0, chunking is disabled entirely for newly-uploaded NARs.
+        # If 1, all NARs are chunked.
+        nar-size-threshold = 64 * 1024; # 64 KiB
+
+        # The preferred minimum size of a chunk, in bytes
+        min-size = 16 * 1024; # 16 KiB
+
+        # The preferred average size of a chunk, in bytes
+        avg-size = 64 * 1024; # 64 KiB
+
+        # The preferred maximum size of a chunk, in bytes
+        max-size = 256 * 1024; # 256 KiB
+      };
+    };
   };
 }

286eda13c7704f038319fd385c74ac3d02347cf9
Willy <jemand771@gmx.net>
2024-11-20 19:21:42 +0100

    don't output quotes from jq

--- build-all.fish
+++ build-all.fish
@@ -4,7 +4,7 @@ set fish_trace 1
 
 mkdir -p out
 
-for config in $(nix flake show --json | jq ".nixosConfigurations | keys[]")
+for config in $(nix flake show --json | jq -r ".nixosConfigurations | keys[]")
   nom build .#nixosConfigurations.$config.config.system.build.toplevel -o out/$config || exit 1
   attic push cache result || exit 1
 end

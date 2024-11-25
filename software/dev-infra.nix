e22794bcc786370721b19f4c921c2534a8a8e6fb
Willy <jemand771@gmx.net>
2024-11-25 21:27:49 +0100

    dev-infra: add quickemu + spice support

--- software/dev-infra.nix
+++ software/dev-infra.nix
@@ -2,27 +2,32 @@
 
 {
   options.jemand771.dev-infra.enable = lib.mkEnableOption "infra development";
-  config.environment.systemPackages = with pkgs; lib.mkIf config.jemand771.dev-infra.enable [
-    argocd
-    cilium-cli
-    k9s
-    kube-capacity
-    kube-linter
-    kubernetes-helm
-    kubectl
-    kubectl-cnpg
-    unstable.kubectl-df-pv
-    kubeseal
-    kustomize
-    minikube
-    # TODO headlamp desktop file
-    (pkgs.appimageTools.wrapType1 {
-      name = "headlamp";
-      src = pkgs.fetchurl {
-        url = "https://github.com/headlamp-k8s/headlamp/releases/download/v0.22.0/Headlamp-0.22.0-linux-x64.AppImage";
-        hash = "sha256-KA4lxfiZLkkMANOg77n9mANTYXI+BJRwGUa1TRalFGM=";
-      };
-    })
-    opentofu
-  ];
+  config = lib.mkIf config.jemand771.dev-infra.enable {
+    environment.systemPackages = with pkgs; [
+      argocd
+      cilium-cli
+      k9s
+      kube-capacity
+      kube-linter
+      kubernetes-helm
+      kubectl
+      kubectl-cnpg
+      unstable.kubectl-df-pv
+      kubeseal
+      kustomize
+      minikube
+      # TODO headlamp desktop file
+      (pkgs.appimageTools.wrapType1 {
+        name = "headlamp";
+        src = pkgs.fetchurl {
+          url = "https://github.com/headlamp-k8s/headlamp/releases/download/v0.22.0/Headlamp-0.22.0-linux-x64.AppImage";
+          hash = "sha256-KA4lxfiZLkkMANOg77n9mANTYXI+BJRwGUa1TRalFGM=";
+        };
+      })
+      opentofu
+      spice-gtk
+      quickemu
+    ];
+    virtualisation.spiceUSBRedirection.enable = true;
+  };
 }

821cd5ee089650780bb23cf3fde7a0526723e337
Willy <jemand771@gmx.net>
2025-03-26 20:47:17 +0100

    nixbox: wayland time

--- hosts/nixbox.nix
+++ hosts/nixbox.nix
@@ -12,12 +12,9 @@
 
   networking.hostName = "nixbox"; # Define your hostname.
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.displayManager.defaultSession = "plasmax11";
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   # Enable CUPS to print documents.

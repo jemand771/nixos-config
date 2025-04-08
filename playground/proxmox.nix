42137e42b318d935c63bf576953815bc2c2b337b
Willy <jemand771@gmx.net>
2025-04-08 23:37:19 +0200

    proxmox: try disko "single-disk-ext4" template

--- playground/proxmox.nix
+++ playground/proxmox.nix
@@ -28,6 +28,16 @@ inputs.nixpkgs.lib.nixosSystem {
                   size = "1M";
                   type = "EF02"; # for grub MBR
                 };
+                ESP = {
+                  size = "1G";
+                  type = "EF00";
+                  content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot";
+                    mountOptions = [ "umask=0077" ];
+                  };
+                };
                 root = {
                   size = "100%";
                   content = {

99b23d2864dfb1f95732595cbab3a0eac2461d7a
Willy <jemand771@gmx.net>
2025-05-18 20:26:58 +0200

    move git to nixos option, enable lfs

--- software/basics.nix
+++ software/basics.nix
@@ -10,11 +10,16 @@
   config.environment.systemPackages =
     with pkgs;
     lib.mkIf config.jemand771.basics.enable [
-      git
       google-chrome
       (unstable-small.discord.override {
         withOpenASAR = true;
         withVencord = true;
       })
     ];
+  config.programs = lib.mkIf config.jemand771.basics.enable {
+    git = {
+      enable = true;
+      lfs.enable = true;
+    };
+  };
 }

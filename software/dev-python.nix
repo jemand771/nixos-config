d2b8e1c4d30aa2306af1ce9f4f2454512daefe8b
Willy <jemand771@gmx.net>
2025-12-02 23:06:14 +0100

    add timewriter service for use with obs
    looks, I will stop adding random garbage into dev-python one day. but that day is _not_ today

--- software/dev-python.nix
+++ software/dev-python.nix
@@ -35,4 +35,21 @@
       kdePackages.kdenlive
     ];
   config.services.flatpak.enable = lib.mkIf config.jemand771.dev-python.enable true;
+  config.systemd.services.write-current-time = lib.mkIf config.jemand771.dev-python.enable {
+    description = "Write current time to /run/current-time.txt every second";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.writeShellApplication {
+        name = "write-time";
+        text = ''
+          while true; do
+            date +"%H:%M:%S" > /run/current-time.txt
+            sleep 0.1
+          done
+        '';
+      }}/bin/write-time";
+      Restart = "always";
+      RestartSec = 1;
+    };
+  };
 }

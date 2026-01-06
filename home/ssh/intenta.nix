f305f7890d03a957c02b01329a5076f95439a3f5
Willy Hille <willy.hille@intenta.de>
2026-01-06 17:09:08 +0100

    ssh: add sstrint001

--- home/ssh/intenta.nix
+++ home/ssh/intenta.nix
@@ -6,6 +6,9 @@
       user = "root";
       identityFile = "~/.ssh/id_seinf";
     };
+    "sstrint001.intop01.de" = lib.hm.dag.entryBefore [ "*.intop01.de" ] {
+      user = "adm_wihi";
+    };
     # TODO import more stuff
   };
 }

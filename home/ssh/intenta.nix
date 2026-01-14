186242da61a3ee3fe0a15134386f662b043201b9
Willy Hille <willy.hille@intenta.de>
2026-01-14 13:05:06 +0100

    ssh/intenta: import remaining special hosts

--- home/ssh/intenta.nix
+++ home/ssh/intenta.nix
@@ -6,9 +6,39 @@
       user = "root";
       identityFile = "~/.ssh/id_seinf";
     };
+    "sftp.intenta.de" = {
+      user = "wihi";
+      identityFile = "~/.ssh/id_sftp";
+    };
+    "sfgz001.intop01.de" = lib.hm.dag.entryBefore [ "*.intop01.de" ] {
+      user = "wihi";
+    };
     "sstrint001.intop01.de" = lib.hm.dag.entryBefore [ "*.intop01.de" ] {
       user = "adm_wihi";
     };
-    # TODO import more stuff
+    rms = {
+      user = "root";
+      hostname = "rms.intenta.de";
+      identityFile = "~/.ssh/id_rms";
+    };
+    rd3 = {
+      user = "ubuntu";
+      hostname = "10.153.12.113";
+      identityFile = "~/.ssh/id_rd";
+    };
+    rd1r = {
+      user = "ubuntu";
+      hostname = "localhost";
+      port = 22118;
+      identityFile = "~/.ssh/id_rd";
+      proxyJump = "rms";
+    };
+    rd4r = {
+      user = "ubuntu";
+      hostname = "localhost";
+      port = 22193;
+      identityFile = "~/.ssh/id_rd";
+      proxyJump = "rms";
+    };
   };
 }

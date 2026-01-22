8200409e44037d6993a80d89dc91a825a279f71f
Willy <jemand771@gmx.net>
2026-01-23 00:01:03 +0100

    update build-all.fish to allow IFD

--- build-all.fish
+++ build-all.fish
@@ -4,7 +4,8 @@ set fish_trace 1
 
 mkdir -p out
 
-for config in $(nix flake show --json | jq -r ".nixosConfigurations | keys[]")
+for config in $(nix eval --impure --json --expr 'builtins.attrNames (builtins.getFlake "/etc/nixos").nixosConfigurations' | jq -r ". | values[]")
   nom build .#nixosConfigurations.$config.config.system.build.toplevel -o out/$config || exit 1
   attic push cache out/$config || exit 1
+  rm -f out/$config
 end

bd204289eb389d2767bc280e1b91c0884e1967a4
Willy Hille <willy.hille@intenta.de>
2026-01-30 12:33:48 +0100

    comment what all patches do

--- patches.nix
+++ patches.nix
@@ -10,12 +10,12 @@ let
   npr = pr "NixOS/nixpkgs";
   patches = {
     nixpkgs = [
-      (npr 423815 "sha256-nlR80hiNLmLAOLo4RbIzzcFWTcDykiaz/KWZWJL2l5M")
-      (npr 483377 "sha256-Pw4VKtukEkxy1UIOlo2+cnTnfKR61HTwk3n6BkU6OX8=")
+      (npr 423815 "sha256-nlR80hiNLmLAOLo4RbIzzcFWTcDykiaz/KWZWJL2l5M") # cibuildwheel
+      (npr 483377 "sha256-Pw4VKtukEkxy1UIOlo2+cnTnfKR61HTwk3n6BkU6OX8=") # vencord
       (pkgs.fetchpatch {
         url = "https://github.com/NixOS/nixpkgs/compare/master...jemand771:nixpkgs:jenkins.diff";
         hash = "sha256-/huCC+rIOPmzxXC5VWyIElTmhuBxWpVWrxR7ct2KowQ=";
-      })
+      }) # jenkins plugins
     ];
   };
 in

9a9614b2b5010ed920e7c8f26d9c172a2b60c154
Willy <jemand771@gmx.net>
2026-05-05 21:16:09 +0200

    unfuck secrets on nixbox

--- secrets-nixos.nix
+++ secrets-nixos.nix
@@ -3,7 +3,7 @@
 {
   age.secrets.restic-password.file = ./secrets/restic-password.age;
   age.secrets.github-mcp-pat = {
-    file = ../secrets/github-mcp-pat.age;
+    file = secrets/github-mcp-pat.age;
     owner = "willy";
   };
 }

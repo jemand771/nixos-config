a1d789ab0a24f72602822cbef95ab0be2e91e825
Willy <jemand771@gmx.net>
2026-06-27 02:48:45 +0200

    nvidiagpu: add nixos cuda team cache

--- modules/nixos/nvidiagpu.nix
+++ modules/nixos/nvidiagpu.nix
@@ -54,5 +54,10 @@
       cudaForwardCompat = true;
       allowUnfreePredicate = pkgs._cuda.lib.allowUnfreeCudaPredicate;
     };
+
+    nix.settings = {
+      substituters = [ "https://cache.nixos-cuda.org" ];
+      trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+    };
   };
 }

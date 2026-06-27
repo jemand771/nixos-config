7fb5650cb45e01f4b688f8746f5cc6efa0822871
Willy <jemand771@gmx.net>
2026-06-27 23:10:15 +0200

    patch nixpkgs for faster blender builds

--- patches.nix
+++ patches.nix
@@ -15,6 +15,7 @@ let
         url = "https://github.com/NixOS/nixpkgs/compare/master...jemand771:nixpkgs:jenkins.diff";
         hash = "sha256-/huCC+rIOPmzxXC5VWyIElTmhuBxWpVWrxR7ct2KowQ=";
       }) # jenkins plugins
+      (npr 535873 "sha256-ajat+hRHf17wMmjebBlpWE5eC/DODBWgcoXeoNE+ku0=") # blender cudaCapabilities
     ];
   };
 in

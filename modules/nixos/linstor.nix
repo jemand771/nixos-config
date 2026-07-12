95b429d9ea9444bf7c54195573ff4134488df09c
Willy <jemand771@gmx.net>
2026-07-12 23:50:28 +0200

    linstor: migrate to mkMerge
    controller module and non-controller (satellite/client) module will need to share some stuff, this'll make it easier to keep track

--- modules/nixos/linstor.nix
+++ modules/nixos/linstor.nix
@@ -18,54 +18,60 @@
       ];
     };
   };
-  config = lib.mkIf config.jemand771.linstor.enable {
-    assertions = [
+  config = lib.mkIf config.jemand771.linstor.enable (
+    lib.mkMerge [
       {
-        assertion = builtins.length config.jemand771.linstor.controllers > 0;
-        message = "need at least one linstor controller";
+        assertions = [
+          {
+            assertion = builtins.length config.jemand771.linstor.controllers > 0;
+            message = "need at least one linstor controller";
+          }
+        ];
+
+        preservation.preserveAt."/persist" = {
+          directories = [
+            # TODO needed?
+            # "/var/lib/linstor"
+            # "/var/lib/linstor.d"
+          ];
+        };
+
+        environment.systemPackages = [ pkgs.linstor-client ];
+        environment.etc."linstor/linstor-client.conf".text = ''
+          [global]
+          controllers = ${lib.concatStringsSep "," config.jemand771.linstor.controllers}
+        '';
+
+        services.drbd = {
+          enable = true;
+          config = ''
+            include "/var/lib/linstor.d/*.res";
+            global {
+              usage-count no;
+              udev-always-use-vnr;
+            }
+          '';
+        };
+        # we just want the system config goodies, not the actual service (it runs `drbdadm up all`, ugh)
+        systemd.suppressedSystemUnits = [ "drbd.service" ];
+
+        systemd.services.linstor-satellite = {
+          description = "LINSTOR satellite";
+          wantedBy = [ "multi-user.target" ];
+          after = [ "network-online.target" ];
+          wants = [ "network-online.target" ];
+          serviceConfig = {
+            ExecStart = lib.getExe' pkgs.linstor-server "linstor-satellite";
+            Restart = "on-failure";
+            StateDirectory = "linstor.d";
+          };
+        };
       }
-    ];
 
-    preservation.preserveAt."/persist" = {
-      directories = [
-        # TODO needed?
-        # "/var/lib/linstor"
-        # "/var/lib/linstor.d"
-      ];
-    };
-
-    environment.systemPackages = [ pkgs.linstor-client ];
-    environment.etc."linstor/linstor-client.conf".text = ''
-      [global]
-      controllers = ${lib.concatStringsSep "," config.jemand771.linstor.controllers}
-    '';
-
-    services.drbd = {
-      enable = true;
-      config = ''
-        include "/var/lib/linstor.d/*.res";
-        global {
-          usage-count no;
-          udev-always-use-vnr;
-        }
-      '';
-    };
-    # we just want the system config goodies, not the actual service (it runs `drbdadm up all`, ugh)
-    systemd.suppressedSystemUnits = [ "drbd.service" ];
-
-    systemd.services.linstor-satellite = {
-      description = "LINSTOR satellite";
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network-online.target" ];
-      wants = [ "network-online.target" ];
-      serviceConfig = {
-        ExecStart = lib.getExe' pkgs.linstor-server "linstor-satellite";
-        Restart = "on-failure";
-        StateDirectory = "linstor.d";
-      };
-    };
-
-    # controller
-    # reactor (+promoters)
-  };
+      {
+        # controller
+        # reactor (+promoters)
+      }
+    ]
+  );
 }

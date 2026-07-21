88af14074a29d658c3e75cc0cdaadfe86342b1a7
Willy Hille <willy.hille@intenta.de>
2026-07-21 14:01:01 +0200

    dev-infra: use headlamp from nixpkgs

--- software/dev-infra.nix
+++ software/dev-infra.nix
@@ -23,15 +23,7 @@
       kubeseal
       kustomize
       minikube
-      # TODO headlamp desktop file
-      (pkgs.appimageTools.wrapType1 {
-        pname = "headlamp";
-        version = "0.22.0";
-        src = pkgs.fetchurl {
-          url = "https://github.com/headlamp-k8s/headlamp/releases/download/v0.22.0/Headlamp-0.22.0-linux-x64.AppImage";
-          hash = "sha256-KA4lxfiZLkkMANOg77n9mANTYXI+BJRwGUa1TRalFGM=";
-        };
-      })
+      headlamp
       opentofu
       postgresql
       pv-migrate

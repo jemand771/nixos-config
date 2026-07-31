a3169b49a1c5d2f96dfe9910736d96489e4b92d7
Willy <jemand771@gmx.net>
2026-07-31 20:41:57 +0200

    nix flake update

--- patches.nix
+++ patches.nix
@@ -15,8 +15,6 @@ let
         url = "https://github.com/NixOS/nixpkgs/compare/master...jemand771:nixpkgs:jenkins.diff";
         hash = "sha256-/huCC+rIOPmzxXC5VWyIElTmhuBxWpVWrxR7ct2KowQ=";
       }) # jenkins plugins
-      (npr 545542 "sha256-5U2DTTLvvCOTQJd3wTJIzL/1HFdX0u7w4XGKTOAR0cw=") # CUDAToolkit_ROOT
-      (npr 547077 "sha256-JdSO+k6+KottM99+7K5vU2J2d9/4DiRKn0CdEck9pIc=") # onnxruntime
     ];
   };
 in

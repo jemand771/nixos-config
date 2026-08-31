8ad6a9facf93a5d96f01d8ed6f928bcbef735081
Willy <jemand771@gmx.net>
2026-08-31 22:24:51 +0200

    unbeatable: explicitly set dns resolvers for VMs

--- modules/nixos/unbeatable.nix
+++ modules/nixos/unbeatable.nix
@@ -120,8 +120,7 @@ in
           "ipv6.gateway" = "2a01:4f8:fff0:18a::1/64";
           "ipv6.routes" = "2a01:4f8:fff0:18a::/64";
           "ipv6.ovn.ranges" = "2a01:4f8:fff0:18a::10-2a01:4f8:fff0:18a::1f";
-          # TODO is this required?
-          # "dns.nameservers" = "185.12.64.1,185.12.64.2,2a01:4ff:ff00::add:1,2a01:4ff:ff00::add:2";
+          "dns.nameservers" = "185.12.64.1,185.12.64.2,2a01:4ff:ff00::add:1,2a01:4ff:ff00::add:2";
         };
       }
       {
